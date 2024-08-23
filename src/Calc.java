9078786def6904a5c52a42259fd8a9edb55a63ce
Paris Papadopoulos <parisppdo@protonmail.com>
2024-08-23 14:29:01 +0300

    added button labels and field names

--- src/Calc.java
+++ src/Calc.java
@@ -2,14 +2,29 @@ import javax.swing.*;
 
 public class Calc extends JFrame {
     private JPanel Calc;
-    private JButton button1;
-    private JButton button2;
-    private JTextField textField1;
-    private JButton button3;
-    private JButton button4;
-    private JButton button5;
-    private JButton button6;
-    private JButton button7;
+    private JButton bu3;
+    private JButton bu6;
+    private JTextField tf;
+    private JButton bu7;
+    private JButton budot;
+    private JButton bu0;
+    private JButton buand;
+    private JButton budel;
+    private JButton busub;
+    private JButton bumulti;
+    private JButton budiv;
+    private JButton bu8;
+    private JButton bu9;
+    private JButton bu4;
+    private JButton bu5;
+    private JButton bu1;
+    private JButton bu2;
+    private JButton buans;
+    private JButton buc;
+
+    String operators = null;
+    double number1 = 0, number2 = 0;
+
 
     public Calc() {
         setContentPane(Calc);
